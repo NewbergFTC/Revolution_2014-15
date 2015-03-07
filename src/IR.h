7287d8f74b236964fbcdbea498526ebbeeb18390
NewbergFTC <RevolutionNewbergFTC@gmail.com>
2015-03-06 21:28:20 -0800

    Some changes before state.

--- src/IR.h
+++ src/IR.h
@@ -4,15 +4,19 @@
 #include "Util.h"
 
 uint IR;
+uint IR2;
 
 task Start_IR()
 {
     IR = 0;
+    IR2 = 0;
 
     while(1)
     {
         IR = SensorValue[IR_Sensor];
+        IR2 = SensorValue[IR_Back];
         nxtDisplayBigTextLine(0, "IR: %i", IR);
+        nxtDisplayBigTextLine(6, "IR2: %i", IR2);
     }
 }
 
